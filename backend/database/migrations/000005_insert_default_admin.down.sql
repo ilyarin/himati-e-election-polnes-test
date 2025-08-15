ffbac4de4c3cc50311892c467cf9ce79e077b217
ilyarin <syahdidian@gmail.com>
2025-08-15 09:37:46 +0800

    Update

--- backend/database/migrations/000005_insert_default_admin.down.sql
+++
@@ -1,1 +0,0 @@
-DELETE FROM users WHERE full_name = 'Default Admin' AND role = 'admin';
--- backend/database/migrations/000005_insert_default_admin.up.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (full_name, password, role, phone_number) 
-VALUES ('Default Admin', '$2a$10$Wzbqy0odFDjfVAhTmJ9JheMdiAg02svR3gnb1KC4bO02HG2ocCLWy', 'admin', '085285923047');
